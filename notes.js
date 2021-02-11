64c3b6b193c3e01f8b7343bbb2191e4647d9c3f6
chandan <chjain99@in.ibm.com>
2021-02-11 19:48:52 +0530

    fix for body

--- notes.js
+++ notes.js
@@ -1,27 +1,27 @@
-const fs = require('fs');
-const chalk = require('chalk');
+const fs = require("fs");
+const chalk = require("chalk");
 
 function loadNotes() {
-	try {
-		return JSON.parse(fs.readFileSync('./notes.json').toString());
-	} catch (error) {
-		return [];
-	}
+  try {
+    return JSON.parse(fs.readFileSync("./notes.json").toString());
+  } catch (error) {
+    return [];
+  }
 }
 
-function addNotes(title, content) {
-	const notes = loadNotes();
-	const duplicateNote = notes.find(note => note.title === title);
-	if (!duplicateNote) {
-		notes.push({
-			title: title,
-			content: content
-		});
-		console.log(chalk.green.inverse('Note Added!'));
-	} else {
-		console.log(chalk.yellow.inverse('Note Title taken!'));
-	}
-	saveNotes(notes);
+function addNotes(title, body) {
+  const notes = loadNotes();
+  const duplicateNote = notes.find((note) => note.title === title);
+  if (!duplicateNote) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    console.log(chalk.green.inverse("Note Added!"));
+  } else {
+    console.log(chalk.yellow.inverse("Note Title taken!"));
+  }
+  saveNotes(notes);
 }
 
 // function deleteNote(title) {
@@ -33,48 +33,50 @@ function addNotes(title, content) {
 // }
 
 function saveNotes(notes) {
-	fs.writeFileSync('./notes.json', JSON.stringify(notes));
+  fs.writeFileSync("./notes.json", JSON.stringify(notes));
 }
 
 function getNotes(title) {
-	const notes = loadNotes();
-	const noteToBeRead = notes.find(
-		note =>
-			note.title.toString().toLowerCase() === title.toString().toLowerCase()
-	);
-	if (noteToBeRead) {
-		console.log(chalk.blue.inverse('Title:- ' + noteToBeRead.title));
-		console.log('Content:- ' + noteToBeRead.content);
-	} else {
-		console.log(chalk.yellow.inverse('No such note exists'));
-	}
+  const notes = loadNotes();
+  const noteToBeRead = notes.find(
+    (note) =>
+      note.title.toString().toLowerCase() === title.toString().toLowerCase()
+  );
+  if (noteToBeRead) {
+    console.log(chalk.blue.inverse("Title:-", noteToBeRead.title));
+    console.log(chalk.blue.inverse("Body:-", noteToBeRead.body));
+  } else {
+    console.log(chalk.yellow.inverse("No such note exists"));
+  }
 }
 
 function listNotes() {
-	const notes = loadNotes();
-	if (notes.length === 0) {
-		console.log(chalk.red.inverse('Notes List Empty'));
-	} else {
-		console.log(chalk.green.inverse('My notes List'));
-		notes.forEach((element, index) =>
-			console.log(chalk.blue.inverse(index + 1 + '.' + element.title))
-		);
-	}
+  const notes = loadNotes();
+  if (notes.length === 0) {
+    console.log(chalk.red.inverse("Notes List Empty"));
+  } else {
+    console.log(chalk.green.inverse("My notes List"));
+    notes.forEach((element, index) =>
+      console.log(
+        chalk.blue.inverse(index + 1 + ".", element.title, "-", element.body)
+      )
+    );
+  }
 }
 
 function deleteNote(title) {
-	const notes = loadNotes();
-	const noteToBeDeleted = notes.find(
-		note =>
-			note.title.toString().toLowerCase() === title.toString().toLowerCase()
-	);
-	if (!noteToBeDeleted) {
-		console.log(chalk.yellow.inverse('No such note exists'));
-	} else {
-		notes.splice(notes.indexOf(noteToBeDeleted), 1);
-		console.log(chalk.red.inverse('Note Deleted'));
-	}
-	saveNotes(notes);
+  const notes = loadNotes();
+  const noteToBeDeleted = notes.find(
+    (note) =>
+      note.title.toString().toLowerCase() === title.toString().toLowerCase()
+  );
+  if (!noteToBeDeleted) {
+    console.log(chalk.yellow.inverse("No such note exists"));
+  } else {
+    notes.splice(notes.indexOf(noteToBeDeleted), 1);
+    console.log(chalk.red.inverse("Note Deleted"));
+  }
+  saveNotes(notes);
 }
 
 module.exports = { getNotes, addNotes, deleteNote, listNotes };
